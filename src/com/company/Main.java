31f64a2604e9f67a75e84770a3f7384c8b90a31e
MarcusEvans03 <marcusevans03@gmail.com>
2018-04-02 19:14:01 -0500

    Creating instance variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,26 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    
 
     public static void main(String[] args) {
         System.out.println("\nHello Grocery Shopping App\n");
 
 
-        GroceryList groceries = new GroceryList();
+        GroceryItem groceries = new GroceryItem();
+
+        Random rand = new Random();
+
+        ArrayList<String> groceryItems = new ArrayList<>();
+        groceryItems.add("Milk");
+        groceryItems.add("Eggs");
+        groceryItems.add("Ham");
+
+        System.out.println(groceryItems);
 
 
 
